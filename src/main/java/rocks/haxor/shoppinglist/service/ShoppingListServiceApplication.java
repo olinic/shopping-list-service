2b6a5a755bb4bb2a3380d564e8cae86b1f914b41
Oliver <oliver.nic012@gmail.com>
2024-06-10 11:26:23 -0500

    Setup

---
+++ src/main/java/rocks/haxor/shoppinglist/service/ShoppingListServiceApplication.java
@@ -0,0 +1,13 @@
+package rocks.haxor.shoppinglist.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingListServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingListServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rocks/haxor/shoppinglist/service/ShoppingListServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.haxor.shoppinglist.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingListServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
