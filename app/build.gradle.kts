bd8d7458fcee00bab7f85343249affc51c62edb3
Oliver <oliver.nic012@gmail.com>
2024-07-01 13:55:29 -0500

    Setup dev and prod properties.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,8 @@ tasks.named<io.micronaut.gradle.docker.NativeImageDockerfile>("dockerfileNative"
     args(
         "-XX:MaximumHeapSizePercent=80",
         "-Dio.netty.allocator.numDirectArenas=0",
-        "-Dio.netty.noPreferDirect=true"
+        "-Dio.netty.noPreferDirect=true",
+        "-Dmicronaut.environments=prod"
     )
 }
 
